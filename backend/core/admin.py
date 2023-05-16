d759809b5e49d9d7dd23986412f6e17365e49109
Filip <filipelblag@gmail.com>
2023-05-16 20:07:27 +0200

    created Task model

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+from .models import Task
+
+
+class TaskAdmin(ModelAdmin):
+    pass
+
+
+admin.site.register(Task, TaskAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
