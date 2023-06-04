b428aee9a005d908185ffc6cf67eee995a6e49b6
Filip <filipelblag@gmail.com>
2023-06-04 18:33:29 +0200

    implemented and tested users fixture

--- backend/core/tests/test_models.py
+++ backend/core/tests/test_models.py
@@ -1,6 +1,10 @@
+import json
+
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
+from django.core.management import call_command
 
 User = get_user_model()
 
@@ -32,3 +36,14 @@ def test_create_user(create_user, username, password):
 def test_create_user_no_username(create_user):
     with pytest.raises(TypeError) as e:
         create_user()
+
+
+@pytest.mark.django_db
+def test_users_command_fixture():
+    with open(settings.BASE_DIR / "core/fixtures/users.json") as fixture_file:
+        data = json.loads(fixture_file.read())
+        users_count = len(data)
+
+    assert User.objects.count() == 0
+    call_command("loaddata", "users.json")
+    assert User.objects.count() == users_count
